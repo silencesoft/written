74456e70f314b4c7264bad30c5786dff094dd3db
Bh <bh@axul.net>
2023-03-21 23:13:02 -0500

    Embed other references in post.

--- src/components/Post/Content/index.tsx
+++ src/components/Post/Content/index.tsx
@@ -1,10 +1,10 @@
 import { Container } from '@nextui-org/react';
-import { Metadata, useNostrEvents } from 'nostr-react';
 import { nip19 } from 'nostr-tools';
 import React, { useEffect, useState } from 'react';
 import { useRemark } from 'react-remark';
 
 import DoLink from '@/components/General/DoLink';
+import { useUpdateContent } from '@/hooks/useUpdateContent';
 import { Refs } from '@/interfaces/posts/post';
 
 type Props = {
@@ -26,91 +26,15 @@ const Content: React.FC<Props> = ({ content, pRefs, eRefs, aRefs }: Props) => {
 
     content = content.replace(search, `[${eRef.value}]`);
   });
+  aRefs?.forEach((aRef) => {
+    const custom = aRef.value.split(':');
+    const search = `#[${aRef.pos}]`;
+
+    content = content.replace(search, `[${custom[2]}]`);
+  });
   const [output, setOutput] = useState(content);
-  const [authorsList, setAuthorsList] = useState(pRefs?.map((pRef) => pRef.value) || []);
-  const { events: authors } = useNostrEvents({
-    filter: {
-      authors: authorsList,
-      kinds: [0],
-    },
-  });
-  const [force, setForce] = useState(true);
-  const eventsList = eRefs?.map((eRef) => eRef.value);
-  const { events: notes } = useNostrEvents({
-    filter: {
-      ids: eventsList,
-      kinds: [1],
-    },
-  });
 
-  useEffect(() => {
-    if (!force) return;
-
-    const updateValue = () => {
-      let replaced = output;
-
-      if (authorsList?.length && authors.length > authorsList?.length - 10) {
-        authors.forEach((author) => {
-          const value: Refs[] = pRefs?.filter((pRef) => pRef.value === author.pubkey) || ([] as Refs[]);
-          const userData: Metadata = JSON.parse(author.content);
-
-          if (value?.length) {
-            const search = `[${value[0].value}](nostr`;
-
-            if (userData?.name) {
-              replaced = replaced.replaceAll(search, `[@${userData.name}](nostr`);
-
-              const searchImage = `[${value[0].value}.image]`;
-
-              replaced = replaced.replaceAll(
-                searchImage,
-                `<img alt="@${userData.name}" src="${userData?.picture}" width="200" />`
-              );
-            }
-          }
-        });
-
-        setOutput(replaced);
-
-        if (authors.length === authorsList?.length) {
-          setForce(false);
-        }
-      }
-    };
-
-    updateValue();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authors.length, force]);
-
-  useEffect(() => {
-    const updateValue = () => {
-      let replaced = output;
-
-      if (eventsList?.length && notes.length > eventsList?.length - 10) {
-        notes.forEach((event) => {
-          const value: Refs[] = eRefs?.filter((eRef) => eRef.value === event.id) || ([] as Refs[]);
-
-          if (value?.length) {
-            const search = `[${value[0].value}]`;
-            replaced = replaced.replace(
-              search,
-              `>[${event.pubkey}](nostr:${event.pubkey})\n>\n>${event.content}\n>\n>\n>[${nip19.noteEncode(
-                event.id
-              )}](nostr:${nip19.noteEncode(event.id)})`
-            );
-
-            setAuthorsList([...authorsList, ...[event.pubkey]]);
-            setForce(true);
-          }
-        });
-
-        setOutput(replaced);
-      }
-    };
-
-    updateValue();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [notes.length]);
+  useUpdateContent({ output, setOutput, aRefs, pRefs, eRefs });
 
   useEffect(() => {
     setMarkdownSource(output);
---
+++ src/hooks/useUpdateContent.ts
@@ -0,0 +1,164 @@
+import { Metadata, useNostrEvents } from 'nostr-react';
+import { nip19 } from 'nostr-tools';
+import { useEffect, useState } from 'react';
+
+import { Refs } from '@/interfaces/posts/post';
+
+type Props = {
+  output: string;
+  setOutput: (value: string) => void;
+  aRefs?: Refs[];
+  eRefs?: Refs[];
+  pRefs?: Refs[];
+};
+
+export const useUpdateContent = ({ output, setOutput, aRefs, eRefs, pRefs }: Props) => {
+  const [authorsList, setAuthorsList] = useState(pRefs?.map((pRef) => pRef.value) || []);
+  const { events: authors } = useNostrEvents({
+    filter: {
+      authors: authorsList,
+      kinds: [0],
+    },
+  });
+  const [force, setForce] = useState(true);
+  const eventsList: string[] = eRefs?.map((eRef) => eRef.value) || [];
+
+  const { events: notes } = useNostrEvents({
+    filter: {
+      ids: eventsList,
+      kinds: [1],
+    },
+  });
+  const kindsList: number[] = [];
+  const othersList: string[] =
+    aRefs?.map((aRef) => {
+      const custom = aRef.value.split(':');
+
+      if (!kindsList.includes(parseInt(custom[0]))) {
+        kindsList.push(parseInt(custom[0]));
+      }
+
+      return custom[2];
+    }) || [];
+  const { events: others } = useNostrEvents({
+    filter: {
+      kinds: kindsList,
+      ...(othersList.length && { '#d': othersList }),
+    },
+  });
+
+  useEffect(() => {
+    if (!force) return;
+
+    const updateValue = () => {
+      let replaced = output;
+
+      if (authorsList?.length && authors.length > authorsList?.length - 10) {
+        authors.forEach((author) => {
+          const value: Refs[] = pRefs?.filter((pRef) => pRef.value === author.pubkey) || ([] as Refs[]);
+          const userData: Metadata = JSON.parse(author.content);
+
+          if (value?.length) {
+            const search = `[${value[0].value}](nostr`;
+
+            if (userData?.name) {
+              replaced = replaced.replaceAll(search, `[@${userData.name}](nostr`);
+
+              const searchImage = `[${value[0].value}.image]`;
+
+              replaced = replaced.replaceAll(
+                searchImage,
+                `<img alt="@${userData.name}" src="${userData?.picture}" width="200" />`
+              );
+            }
+          }
+        });
+
+        setOutput(replaced);
+
+        if (authors.length === authorsList?.length) {
+          setForce(false);
+        }
+      }
+    };
+
+    updateValue();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authors.length, force]);
+
+  useEffect(() => {
+    const updateValue = () => {
+      let replaced = output;
+
+      if (eventsList?.length && notes.length > eventsList?.length - 10) {
+        notes.forEach((event) => {
+          const value: Refs[] = eRefs?.filter((eRef) => eRef.value === event.id) || ([] as Refs[]);
+
+          if (value?.length) {
+            const search = `[${value[0].value}]`;
+            replaced = replaced.replace(
+              search,
+              `>>[${event.pubkey}](nostr:${event.pubkey})\n>>\n>>${event.content}\n>>\n>>\n>>[${nip19.noteEncode(
+                event.id
+              )}](nostr:${nip19.noteEncode(event.id)})`
+            );
+
+            setAuthorsList([...authorsList, ...[event.pubkey]]);
+            setForce(true);
+          }
+        });
+
+        setOutput(replaced);
+      }
+    };
+
+    updateValue();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [notes.length]);
+
+  useEffect(() => {
+    const updateValue = () => {
+      let replaced = output;
+
+      if (othersList?.length && others.length > othersList?.length - 10) {
+        others.forEach((event) => {
+          const value: Refs[] =
+            aRefs?.filter((aRef) => {
+              const custom = aRef.value.split(':');
+              const slug = event.tags.filter((tag) => tag[0] === 'd');
+
+              return custom[2] === slug[0][1];
+            }) || ([] as Refs[]);
+
+          if (value?.length) {
+            const custom = value[0].value.split(':');
+            const search = `[${custom[2]}]`;
+            const title = event.tags.filter((tag) => tag[0] === 'title');
+
+            replaced = replaced.replace(
+              search,
+              `>>[${title.length ? title[0][1] : ''}](${process.env.NEXT_PUBLIC_URL}/post/${
+                custom[2]
+              })\n>>\n>>\n>>[${nip19.naddrEncode({
+                identifier: custom[2],
+                pubkey: custom[1],
+
+                kind: parseInt(custom[0]),
+              })}](nostr:${nip19.naddrEncode({
+                identifier: custom[2],
+                pubkey: custom[1],
+
+                kind: parseInt(custom[0]),
+              })})`
+            );
+          }
+        });
+
+        setOutput(replaced);
+      }
+    };
+
+    updateValue();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [others.length]);
+};

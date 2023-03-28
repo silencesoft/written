6043a8db0e27771f3d291313b4c83be8125aa921
Bh <bh@axul.net>
2023-03-28 15:02:54 -0500

    Update rss file path.

--- src/utils/createRss.ts
+++ src/utils/createRss.ts
@@ -1,5 +1,6 @@
 import { Feed } from 'feed';
 import fs from 'fs';
+import path from 'path';
 
 import { Post } from '@/interfaces/posts/post';
 
@@ -9,7 +10,8 @@ type Props = {
 
 export const createRss = ({ posts }: Props) => {
   const siteUrl = process.env.NEXT_PUBLIC_URL || '';
-  const file = './public/rss.xml';
+  const publicDirectory = path.resolve(process.cwd(), 'public');
+  const file = `${publicDirectory}/rss.xml`;
 
   const options = {
     title: `${process.env.NEXT_PUBLIC_NAME} posts | RSS Feed`,

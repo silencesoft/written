0c5d04427df4b8c470aeee24331b10b456a1c74f
Bh <bh@axul.net>
2023-03-29 17:52:34 -0500

    Update tag page.

--- src/pages/tag/[tag].tsx
+++ src/pages/tag/[tag].tsx
@@ -1,27 +1,29 @@
 import { useSetAtom } from 'jotai';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 
+import { Filter } from '@/interfaces/nostr/filter';
 import { filterAtom } from '@/state/nostr';
 import Home from '..';
 
-type Props = {};
+type Props = {
+  tag: string;
+};
 
 const Tag: React.FC<Props> = (props: Props) => {
   const router = useRouter();
   const { tag } = router.query;
   const setFilter = useSetAtom(filterAtom);
 
-  useEffect(() => {
-    if (tag) {
-      setFilter({ type: 'tag', value: tag?.toString() });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tag]);
+  if (!tag) {
+    return <></>;
+  }
+
+  const filter: Filter = { type: 'tag', value: tag?.toString() };
 
   return (
     <>
-      <Home embedded={true} />
+      <Home embedded={true} filter={filter} />
     </>
   );
 };
